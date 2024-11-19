94be0291391d2f82aacd776d272d56042fd2432d
Victor Hugo <victorhugomarquezin@gmail.com>
2024-11-19 10:48:44 -0300

    adicionar pagina selecao de sistema petroecol

---
+++ application/views/sistemas.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Petroecol Sistemas</title>
+    <meta charset="utf-8">
+    <!-- Última versão CSS compilada e minificada -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/vendor/icofont/icofont.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/vendor/boxicons/css/boxicons.min.cs') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/vendor/animate.css/animate.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/vendor/venobox/venobox.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/vendor/owl.carousel/assets/owl.carousel.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/vendor/aos/aos.css') ?>" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
+
+</head>
+
+<style>
+    body {
+        background-color: #f5f9fc;
+    }
+</style>
+
+<body>
+
+    <section id="portfolio" class="portfolio section-bg">
+        <div class="container">
+
+            <div class="section-title">
+                <h2>Petroecol Soluções Ambientais</h2>
+                </br></br></br>
+            </div>
+
+
+
+            <div class="row portfolio-container">
+
+                <div class="col-lg-4 col-md-6  filter-card">
+                    <div class="portfolio-wrap">
+                        <img src="<?= base_url('assets/img/sistema_manutencao.png') ?>" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Petroecol Manutenção</h4>
+                            <div class="portfolio-links">
+                                <a href="<?= base_url('admin') ?>"><i class="icofont-world"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6  filter-card">
+                    <div class="portfolio-wrap">
+                        <img src="<?= base_url('assets/img/sistema_financeiro.png') ?>" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Petroecol Controle</h4>
+                            <div class="portfolio-links">
+                                <a href="<?= base_url('financeiro/login') ?>"><i class="icofont-world"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6  filter-card">
+                    <div class="portfolio-wrap">
+                        <img src="<?= base_url('assets/img/sistema_coletas.png') ?>" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Petroecol Coletas</br>(Em Breve)</h4>
+                            
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section><!-- End Our Portfolio Section -->
+
+
+
+    <!-- Vendor JS Files -->
+    <script src="<?= base_url('assets/vendor/jquery/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/jquery.easing/jquery.easing.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/php-email-form/validate.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/jquery-sticky/jquery.sticky.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/venobox/venobox.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/waypoints/jquery.waypoints.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/counterup/counterup.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/owl.carousel/owl.carousel.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/isotope-layout/isotope.pkgd.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/aos/aos.js') ?>"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+
+</body>
+
+
+
+</html>
